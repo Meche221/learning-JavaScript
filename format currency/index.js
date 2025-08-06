f190f26cde4074ceb4b38e5fc8637c8af41fc7ab
Meche221 <binaymeche22@gmail.com>
2025-08-06 14:42:17 +0545

    currency foramting

---
+++ format currency/index.js
@@ -0,0 +1,12 @@
+let number = 123456.789
+
+// number = number.toLocaleString("en-US");
+// number = number.toLocaleString("hi-IN");
+// number = number.toLocaleString("de-DE");
+// number = number.toLocaleString(undefined);
+
+// number = number.toLocaleString("en-US", {style: "currency", currency: "USD"})
+// number = number.toLocaleString("hi-IN", {style: "currency", currency: "INR"})
+number = number.toLocaleString("de-DE", {style: "currency", currency:"EUR"})
+
+console.log(number);
