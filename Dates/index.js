8ed94b122384de76443fcf4a3b0fa936b00eb03e
Meche221 <binaymeche22@gmail.com>
2025-08-06 10:38:47 +0545

    dates

---
+++ Dates/index.js
@@ -0,0 +1,41 @@
+// // Date (year, month, day, hour, minute, second, milisecond)
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const hour = date.getHours();
+// const minute = date.getMinutes();
+// const second = date.getSeconds();
+// const dayOfWeek = date.getDay()
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(hour);
+// console.log(minute);
+// console.log(second);
+// console.log(dayOfWeek);
+
+
+
+
+// const date = new Date();
+
+// date.setFullYear(2024);
+// date.setMonth(8);
+// date.setDate(14);
+// date.getHours(10);
+
+
+// console.log(date);
+
+
+
+
+const date1 = new Date("2022-03-7")
+const date2 = new Date("2024-06-18")
+
+if(date2 > date1){
+    console.log("Happy BirthDay");   
+}
+
