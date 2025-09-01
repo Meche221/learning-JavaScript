2afcc3eadb685727fcd9ca3df841f5f5df5b0e00
Meche221 <binaymeche22@gmail.com>
2025-09-01 20:48:58 +0545

    learning fetch

---
+++ fetch/index.js
@@ -0,0 +1,18 @@
+fetchData();
+
+async function fetchData(){
+
+    try {
+        const response = await fetch ("https://pokeapi.co/api/v2/pokemon/typhlosion");
+
+        if(!response.ok){
+            throw new Error("Could not fetch resources");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
